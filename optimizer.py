d41a5e32bcd654ffc0e0e3dfec55b81d0aeebf90
robbiethompson2018 <rthomp@stanford.edu>
2023-03-01 23:10:59 -0800

    functional adam optimizer

--- optimizer.py
+++ optimizer.py
@@ -41,9 +41,32 @@ class AdamW(Optimizer):
 
                 # State should be stored in this dictionary
                 state = self.state[p]
+                if 'm' not in state.keys():
+                    state['m'] = 0
+                if 'v' not in state.keys():
+                    state['v'] = 0
+                if 't' not in state.keys():
+                    state['t'] = 1
+                # print('grad')
+                # print(grad)
+                # # print('group')
+                # # print(group)
+                # print('p')
+                # print(p)
+                # print(p.data)
+                # print('state')
+                # print(state)
+                # print('self.param_groups')
+                # print(self.param_groups)
+                # print('group[params]')
+                # print(group["params"])
 
                 # Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
+                beta1 = group["betas"][0]
+                beta2 = group["betas"][1]
+                eps = group["eps"]
+                wd = group["weight_decay"]
 
                 # Complete the implementation of AdamW here, reading and saving
                 # your state in the `state` dictionary above.
@@ -51,15 +74,32 @@ class AdamW(Optimizer):
                 # (they are lr, betas, eps, weight_decay, as saved in the constructor).
                 #
                 # 1- Update first and second moments of the gradients
+                firstMoment = beta1 * state['m'] + (1- beta1) * grad
+                secondMoment = beta2 * state['v'] + (1 - beta2) * torch.square(grad)
+
                 # 2- Apply bias correction
                 #    (using the "efficient version" given in https://arxiv.org/abs/1412.6980;
                 #     also given in the pseudo-code in the project description).
+                alpha_t = alpha * math.sqrt((1 - beta2 ** state['t'])) / (1 - beta1 ** state['t'])
                 # 3- Update parameters (p.data).
+                update1 = alpha_t * firstMoment / (torch.sqrt(secondMoment) + eps) #alpha_t?
+                #lambda is weight decay!
+                update2 = p.data * wd * alpha
+                #p.data -= alpha_t * firstMoment / (math.sqrt(secondMoment) + eps)
+                temp = p.data
+                p.data -= update1
                 # 4- After that main gradient-based update, update again using weight decay
                 #    (incorporating the learning rate again).
-
+                # print('updates')
+                # print(update1)
+                # print(update2)
+                p.data -= update2
+                state['t'] += 1
+                state['m'] = firstMoment
+                state['v'] = secondMoment
+                self.state[p] = state
                 ### TODO
-                raise NotImplementedError
+                #raise NotImplementedError
 
 
         return loss
